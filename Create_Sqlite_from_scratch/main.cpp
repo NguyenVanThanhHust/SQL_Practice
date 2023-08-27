5b1cb215a77e8801538b1125ce449ab9b658a8b2
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2023-08-27 19:54:03 +0700

    setup repl in c++

--- Create_Sqlite_from_scratch/main.cpp
+++ Create_Sqlite_from_scratch/main.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 #include <string>
 
-#include "constant.h"
-#include "Pager.h"
-#include "CommandProcessor.h"
+using std::cout;
+using std::cin;
+using std::endl;
+
+struct InputBuffer {
+    std::string buffer;
+    size_t buffer_length=0; 
+};
+
+void print_promt()
+{
+    cout<<"db >";
+};
+
+void read_input(InputBuffer &input_buffer)
+{
+    std::getline(std::cin, input_buffer.buffer);
+    input_buffer.buffer_length = input_buffer.buffer.size();
+};
 
 int main()
 {
-    Table  table;
-    CommandProcessor commandProcessor;
-    commandProcessor.getCommand();
+    InputBuffer input_buffer;
+    while (true)
+    {
+        print_promt();
+        read_input(input_buffer);
+        if (input_buffer.buffer.compare(0, 5, ".exit") == 0)
+        {
+            cout<<"Exiting."<<endl;
+            break;
+        }
+        else
+        {
+            cout<<"Unrecognize command"<<endl;
+        }
+    }
     return 0;
 }
